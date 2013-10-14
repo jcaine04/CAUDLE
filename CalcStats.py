e8426d54bb685d0d6b1f98341cae5afae92166eb
jcaine04 <jcaine04@gmail.com>
2013-10-14 09:21:23 -0400

    updates to CalcStats class

---
+++ CalcStats.py
@@ -0,0 +1,23 @@
+'''
+Created on Oct 13, 2013
+
+@author: johncaine
+'''
+
+class CalcStats(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def twoPtPercentage(self, twopm, twopa):
+        return twopm / twopa
+    
+    def threePtPercentage(self, threepm, threepa):
+        return threepm / threepa
