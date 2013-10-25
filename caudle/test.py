c599e63d52bda975e10c772e31e9d12cad23ef8f
jcaine04 <jcaine04@gmail.com>
2013-10-24 23:32:12 -0400

    more changes and additions to the rewrite

--- caudle/test.py
+++ caudle/test.py
@@ -3,19 +3,46 @@ Created on Oct 19, 2013
 
 @author: johncaine
 '''
-import os
-import sys
-from system import Utility
-from extractstats import TeamsExtract
-from system import DBConnection
+import MySQLdb
 
+class DatabaseManager(object):
+    def __init__(self):
+        try:
+            self.db = MySQLdb.connect(
+                                      host = "localhost",
+                                      user = "root",
+                                      passwd = "password",
+                                      db = "ncbstats"
+            )
+        except:
+            pass
+        
+        self.cur = self.db.cursor()
+        
+    def query(self, arg):
+        try:
+            self.cur.execute(arg)
+        except MySQLdb.Error, e:
+            print "There was an error... %s: %s" % (e.args[0], e.args[1])
+            exit(1)
+        else:    
+            return self.cur
+    
+    def dbinsert(self, arg):
+        try:
+            self.cur.execute(arg)
+        except MySQLdb.Error, e:
+            print "There was an error... %s: %s" % (e.args[0], e.args[1])
+            exit(1)
+        else:
+            self.db.commit()
+            return True
+    
+    def __del__(self):
+        self.cur.close()
+        
+db = DatabaseManager()
+cur = db.query("SELECT name FROM player LIMIT 0,1")
+print cur.fetchone()[0]
 
-#establish db object
-db = DBConnection()
-#establish the main connection and cursor
-cur = db.connect('root', 'password', 'localhost', 'ncbstats')
-u = Utility()
-t = TeamsExtract(cur)
-r = t.getRosterURLs()
-for i in r:
-    print t.get_roster(i)
+            
---
+++ caudle3/stats.py
@@ -0,0 +1,49 @@
+import system
+from bs4 import BeautifulSoup
+import urllib2
+
+class DataExtraction(object):
+    
+    def __init__(self):
+        self.db = system.DatabaseManager()
+        self.util = system.Utilities()
+        self.sys = system.SysConfig()
+        
+    def getGameData(self, url):
+        pass
+    
+    def getTeamUrls(self):
+        
+        #initialize variables
+        url = self.sys.rosterURLSource
+        html = urllib2.urlopen(url)
+        soup = BeautifulSoup(html)
+        
+        #get all the school URLs
+        schoolurls = []
+        schoolTags = soup.find_all(class_='bi')
+        for row in schoolTags:
+            schoolurls.append(row.get('href'))
+       
+        return schoolurls
+    
+    def extractTeamRoster(self, url):
+        pass
+    
+    def getBoxScoreUrls(self, beginDate, endDate):
+        pass
+    
+    def teamDict(self, teamName):
+        pass
+    
+    def playerDict(self, playerName, schoolId):
+        pass
+    
+    def addPlayer(self, playerList):
+        pass
+    
+    def addTeam(self, teamName, conferenceId=-1):
+        pass
+    
+    def addRoster(self, teamId, playerList):
+        pass
